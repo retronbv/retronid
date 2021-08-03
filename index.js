edc629bfdf5e951e0a288f697e5f01d05e1746ca
retronbv <49005044+retronbv@users.noreply.github.com>
2021-08-03 19:31:08 -0400

    new upadtes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { v5 } = require('uuid');
-module.exports.generate = ()=>{
-  uuid = v5(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), "1e230887-bf0e-477d-9f68-a2c27edcbdf6")
-  id = `${uuid.split("-")[0]}${uuid.substr(uuid.length - 2)}`
-  return id
+
+function generateString() {
+  const uuid = v5(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), "1e230887-bf0e-477d-9f68-a2c27edcbdf6")
+  return `${uuid.split("-")[0]}${uuid.substr(uuid.length - 2)}`
+}
+
+module.exports = generateString
+module.exports.generate = generateString
+module.exports.number = (max=10,min=1) => {
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
 }
--- test.js
+++ test.js
@@ -1,2 +1,4 @@
 const retronid = require("./index.js");
 console.log(retronid.generate());
+console.log(retronid.number(0,20));
+console.log(retronid.number())
