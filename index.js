e004b11ca04ca78bb3c4380ba03225f3b581b4ef
retronbv <49005044+retronbv@users.noreply.github.com>
2021-08-03 20:10:50 -0400

    Finish v3 Co-authored-by: RedGuy12 <paul@reid-family.org>

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
-const { v5 } = require('uuid');
+import { v5 } from 'uuid';
 
-function generateString() {
+export function generate() {
   const uuid = v5(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), "1e230887-bf0e-477d-9f68-a2c27edcbdf6")
   return `${uuid.split("-")[0]}${uuid.substr(uuid.length - 2)}`
 }
 
-module.exports = generateString
-module.exports.generate = generateString
-module.exports.number = (max=10,min=1) => {
-  return Math.floor(
-    Math.random() * (max - min) + min
-  )
-=======
-import { v5 } from 'uuid';
-export generate() => {
-  uuid = v5(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5), "1e230887-bf0e-477d-9f68-a2c27edcbdf6")
-  id = `${uuid.split("-")[0]}${uuid.substr(uuid.length - 2)}`
-  return id
->>>>>>> 4ccf26fdb202c1790aad0fd2bcf6082e48cbe818
-}
+export default generate
--- test.js
+++ test.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-const retronid = require("./index.js");
-console.log(retronid.generate());
-console.log(retronid.number(0,20));
-console.log(retronid.number())
-=======
 import { generate } from './index.js'
 
 console.log(generate())
->>>>>>> 4ccf26fdb202c1790aad0fd2bcf6082e48cbe818
